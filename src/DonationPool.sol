f5f0fb81d690eb184ebd6fbf02e6c2a219001239
Allen <allengeorge2400@gmail.com>
2025-07-28 11:33:38 +0530

    Added Donation Pool

---
+++ src/DonationPool.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract DonationPool is Ownable, UUPSUpgradeable {
+    constructor() Ownable(msg.sender) {}
+
+    mapping(address => uint256) tokenDonations;
+    uint256 public nativeDonations;
+
+    function donate(address token, uint256 amount) external {
+        require(IERC20(token).transferFrom(msg.sender,address(this), amount));
+        tokenDonations[token] += amount;
+    }
+
+    function donateNativeEth(uint256 amount) external payable {
+        require(msg.value >0 , "Must send some ETH");
+        nativeDonations += msg.value;
+    }
+
+    function withdrawAllBalance(address to,address token) external onlyOwner{
+        uint256 balance = IERC20(token).balanceOf(address(this));
+        require(balance>0,"No balance to withdraw");
+        require(IERC20(token).transfer(to, balance));
+        tokenDonations[token] = 0;
+    }
+
+    function withdrawNativeEth() external onlyOwner{
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No ETH to withdraw");
+        nativeDonations = 0;
+        payable(owner()).transfer(balance);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal virtual override {}
+
+    receive() external payable {
+        nativeDonations += msg.value;
+    }
+}
--- src/HiringRegistry.sol
+++ src/HiringRegistry.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract HiringRegistry is UUPSUpgradeable,Ownable {
+contract HiringRegistry is UUPSUpgradeable, Ownable {
     struct Skill {
         address issuedTo;
         string skillName;
@@ -17,27 +17,35 @@ contract HiringRegistry is UUPSUpgradeable,Ownable {
     mapping(address => bool) public isGraduate;
     mapping(address => Skill) skills;
     mapping(address => bool) isHired;
-    mapping (address => uint256) hiringQuotes;
+    mapping(address => uint256) hiringQuotes;
 
-    constructor()  Ownable(msg.sender){}
+    constructor() Ownable(msg.sender) {}
 
-    function addGraduate(address graduate, string memory skillName, string memory level, string memory issuer) public onlyOwner{
+    function addGraduate(address graduate, string memory skillName, string memory level, string memory issuer)
+        public
+        onlyOwner
+    {
         if (!isGraduate[graduate]) {
             graduates.push(graduate);
             isGraduate[graduate] = true;
         }
 
         skills[graduate] =
-            Skill({issuedTo: graduate,skillName: skillName, level: level, issuer: issuer, issuedAt: block.timestamp});
-        
+            Skill({issuedTo: graduate, skillName: skillName, level: level, issuer: issuer, issuedAt: block.timestamp});
     }
 
-    function quoteHire(address candidate,uint256 quote) public {
+    function quoteHire(address candidate, uint256 quote) public {
         require(isGraduate[candidate], "Candidate is not a graduate");
         require(!isHired[candidate], "Candidate already Hired");
-
         hiringQuotes[candidate] = quote;
+    }
 
+    function getSkills(address candidate) external view returns (Skill memory) {
+        return skills[candidate];
+    }
+
+    function isCandidateHired(address candidate) external view returns (bool) {
+        isHired[candidate];
     }
 
     function _authorizeUpgrade(address newImplementation) internal virtual override {}
